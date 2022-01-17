6f8e19e1af2e65e26d4182448d59e4fd2dda7d7f
NITISH1-1 <9891nr@gmail.com>
2022-01-17 19:40:37 +0530

    added dark mode in project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import {useState} from "react";
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042842';
+    } else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About US" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About US" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
+        <TextForm mode={mode} heading="Enter the text to analyze below" />
       </div>
     </>
   );
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -24,7 +24,7 @@ export default function TextForm(props) {
   const [text, setText] = useState("");
   return (
     <>
-      <div className="mb-3">
+      <div style={{color: props.mode === 'light' ? 'black': 'white'}} className="mb-3">
         <h1>{props.heading}</h1>
         <textarea
           className="form-control"
@@ -33,6 +33,7 @@ export default function TextForm(props) {
           placeholder="Enter your text"
           onChange={handleOnChange}
           value={text}
+          style={{backgroundColor: props.mode === 'light' ? 'white': 'grey', color: props.mode === 'light' ? 'black': 'white' }}
         ></textarea>
       </div>
       <div className="flex">
@@ -41,12 +42,12 @@ export default function TextForm(props) {
       <button className="btn btn-primary cursor mx-1" onClick={handelExtraSpaces} >Remove Extra Spaces</button>
 
       </div>
-      <div className="container my-3">
-        <h2>Your Text Summary</h2>
+      <div className="container my-3" style={{color: props.mode === 'light' ? 'black': 'white'}}>
+        <h2 >Your Text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes Read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : 'Etner something above to preview it'}</p>
       </div>
     </>
   );
