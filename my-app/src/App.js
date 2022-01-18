2351f3249daad2b8bf2e46e622cf47708ac937cb
NITISH1-1 <9891nr@gmail.com>
2022-01-18 14:21:22 +0530

    added alert

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+
 import {useState} from "react";
 
 function App() {
@@ -10,16 +12,29 @@ function App() {
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042842';
+      showAlert("Dark mode has been enabled", "success")
     } else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success")
+
     }
+  }
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg:message,
+      type: type
+    })
   }
   return (
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About US" />
+      <Alert className="my-3" alert={alert}/>
       <div className="container my-3">
-        <TextForm mode={mode} heading="Enter the text to analyze below" />
+        <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze below" />
       </div>
     </>
   );
