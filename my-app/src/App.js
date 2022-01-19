65cad2aac47c65aacff09180cdab87f3b8b008aa
NITISH1-1 <9891nr@gmail.com>
2022-01-19 23:55:39 +0530

    adding routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import About from "./components/About";
 
 import {useState} from "react";
+
+import { Switch ,Routes, Route, Link, Router } from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -31,11 +35,21 @@ function App() {
   }
   return (
     <>
+    <Router>
+      
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About US" />
       <Alert className="my-3" alert={alert}/>
       <div className="container my-3">
+      <Switch>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/">
         <TextForm showAlert={showAlert} mode={mode} heading="Enter the text to analyze below" />
+        </Route>
+      </Switch>
       </div>
+      </Router>
     </>
   );
 }
